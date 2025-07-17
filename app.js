4f49426b374955fddce079bb7e5ad8ce68522f9a
Eric Huang <erhuang623@gmail.com>
2025-07-16 22:14:16 -0700

    Setup

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-
+var authRouter = require('./routes/auth');
 var app = express();
 
 app.locals.pluralize = require('pluralize');
@@ -23,6 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+app.use('/', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
